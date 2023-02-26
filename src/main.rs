75392ef6f9e0554de02f81c1dcd6a89f092ce0b8
Fermin <ferminoloriz@gmail.com>
2023-02-25 21:13:28 -0600

    render color changes with time and no with cursos position

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,37 @@ fn win32_get_wallclock() -> i64 {
 }
 
 fn render_gradient(buffer: &mut Win32OffscreenBuffer) {
-    let r: i32 = 120;
-    let g: i32 = 200;
-    let b: i32 = 150;
-    let pixels_in_buffer: i32 = buffer.width * buffer.height;
+    static mut WAVE: f32 = 0.001;
+    static mut WAVE_DELTA: f32 = 0.001;
+
+    let r: i32;
+    let g: i32;
+    let b: i32;
+    unsafe {
+        r = (255.0 * WAVE) as i32;
+        g = (255.0 - 255.0 * WAVE) as i32;
+        b = (75.0 + 75.0 * WAVE) as i32;
+    }
 
     buffer.bits.clear();
+    let pixels_in_buffer: i32 = buffer.width * buffer.height;
     for _ in 0..pixels_in_buffer {
         // NOTE(Fermin): Pixel -> BB GG RR AA
         let color: i32 = (b << 24) | (g << 16) | (r << 8) | 255;
         buffer.bits.put_i32(color);
     }
+
+    unsafe { 
+        if WAVE >= 0.99 {
+            WAVE_DELTA = -0.001;
+        }
+        if WAVE <= 0.01 {
+            WAVE_DELTA = 0.001;
+        }
+        WAVE += WAVE_DELTA;
+        println!("wave: {}", WAVE);
+    }
+    
 }
 
 fn main() -> Result<()> {
